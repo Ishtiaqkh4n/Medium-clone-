99b9cb8af0325e4706b66a3c82c2453f67b306b6
Anon873-cyber <async2304@gmail.com>
2026-04-20 03:36:55 -0700

    add Comment model schema with postId, author, and text fields

---
+++ Backend/Models/Comment.Models.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const CommentSchema = new mongoose.Schema({
+ 
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog',
+      required: true
+  },
+    author:{
+        type:String,
+        required:true
+    },
+    text:{
+     type:String,
+        required:true
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model('Comment',CommentSchema)
